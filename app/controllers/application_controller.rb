c0de788efbf22a7e3c9cb7f6077a57271db84726
Im-los <cli10@chicagobooth.edu>
2023-01-25 06:07:38 +0000

    Rock Paper Scissors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,62 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def play_rock
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    @test = @comp_move
+    @test2 = "Nothing2"
+    @test3 = @comp_move
+    @test4 = "Nothing"
+
+    if @comp_move == "rock"
+      @outcome = "tied"
+
+      @test = "Stop 1"
+    elsif @comp_move == "paper"
+      @outcome = "lost"
+
+      @test2 = "stop2"
+    else
+      @outcome = "won"
+    end
+
+    @test4 = @comp_move
+
+    render({ :template => "game_templates/user_rock.html.erb"} )
+  end
+
+  def play_paper
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "paper"
+      @outcome = "tied"
+    elsif @comp_move == "scissors"
+      @outcome = "lost"
+    else
+      @outcome = "won"
+    end
+    
+    render({ :template => "game_templates/user_paper.html.erb"} )
+  end
+
+
+
+  def play_scissors
+    
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "scissors"
+      @outcome = "tied"
+    elsif @comp_move == "rock"
+      @outcome = "lost"
+    else
+      @outcome = "won"
+    end
+    
+    render({ :template => "game_templates/user_scissors.html.erb"} )
+  end
+
+
+
 end
